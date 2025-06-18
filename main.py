9e2e2519f55ada70b4e2f1a7fd7a8f7cb1109799
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-06-18 17:47:58 +0100

    Implement logging in main.py

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 import os
+import logging
 from flask import Flask, request, jsonify
+from logging_config import setup_logging
+
+app_logger = setup_logging()
+app = Flask(__name__)
+main_file_logger = logging.getLogger(__name__)
+
 from gemini_gen_module import GeminiCaller
 from vertex_rag_module import VertexAICaller
 
-app = Flask(__name__)
+main_file_logger.info("Flask app starting up.")
 
 @app.route('/query', methods=['POST'])
 def handle_query():
     """Handles incoming POST requests with a user query, performs RAG, and returns the grounded answer."""
-    print("Entered backend handler successfully.")
 
     if not request.is_json:
-        print("Request not json apparently.")
+        main_file_logger.info("Request not isn't JSON.")
         return jsonify({"error": "Request must be JSON"}), 400
 
     data = request.get_json()
     user_query = data.get('query')
 
     if not user_query:
-        print("No user query apparently.")
+        main_file_logger.info("No user query included.")
         return jsonify({"error": "Missing 'query' in request"}), 400
 
-    print(f"Received query from frontend: '{user_query}'")
+    main_file_logger.info(f"Received query from frontend: '{user_query}'")
 
     vertex_ai_caller = VertexAICaller()
     contexts = vertex_ai_caller.run_vertex_ai_search(user_query)
-    
+
     gemini_caller = GeminiCaller()
     if not contexts:
         # If no context is found, you can still try to answer or inform the user
