c8d01ba4a9b4f8c624df03edfe77014d05c0b81a
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-06-28 17:27:04 +0100

    Rewrite GeminiCaller class to use google.genai package.

--- gemini_gen_module.py
+++ gemini_gen_module.py
@@ -1,16 +1,38 @@
 import logging
 from typing import List, Dict, Any
-from vertexai.generative_models import GenerativeModel
+from google import genai
+from google.genai import types
 
 logger = logging.getLogger(__name__)
 
 class GeminiCaller:
     """Wrapper class to generate responses using Gemini 1.5 Flash, grounded by the retrieved contexts."""
 
-    def __init__(self, model_name):
+    def __init__(self, project_id, location, model_name):
+        self.client = genai.Client(vertexai=True, project=project_id, location=location)
         self.model_name = model_name
+    
+    def generate_response(self, user_query: str, contexts: List[Dict[str, Any]]) -> Dict[str, Any]:
+            """Returns a dictionary with the generated answer and a list of all unique citations."""
+            full_prompt = self._construct_prompt(user_query, contexts)
+            logger.info(f"Gemini prompt length: {len(full_prompt)}...")
 
-    def construct_prompt(self, user_query: str, contexts: List[Dict[str, Any]]) -> str:
+            try:
+                response = self.client.models.generate_content(
+                    model=self.model_name,
+                    contents=full_prompt,
+                    config=self._generate_content_config()
+                )
+
+                generated_text = response.text
+            except Exception as e:
+                logger.exception(f"Error during Gemini generation: {e}")
+                generated_text = "I apologize, but I encountered an error while generating a response."
+
+            logger.info(f"Generated output length: {len(generated_text)}.")
+            return {"response": generated_text,}
+
+    def _construct_prompt(self, user_query: str, contexts: List[Dict[str, Any]]) -> str:
         """String together user query and context into a LLM-ready prompt."""
         prompt_parts = [
             f"Answer the user's question, grounding your answer in the provided context. Do not make up information. If the provided context lacks the answer, state so before answering.",
@@ -24,24 +46,8 @@ class GeminiCaller:
 
         full_prompt = "\n".join(prompt_parts)
         return full_prompt
+    
+    def _generate_content_config(self) -> types.GenerateContentConfig:
+        return types.GenerateContentConfig({"temperature": 0.4, "max_output_tokens": 4000})
 
-    def generate_response(self, user_query: str, contexts: List[Dict[str, Any]]) -> Dict[str, Any]:
-        """Returns a dictionary with the generated answer and a list of all unique citations."""
-        model = GenerativeModel(self.model_name)
-
-        full_prompt = self.construct_prompt(user_query, contexts)
-        logger.info(f"Gemini prompt length: {len(full_prompt)}...")
-
-        try:
-            response = model.generate_content(
-                full_prompt,
-                generation_config={"temperature": 0.4, "max_output_tokens": 1024}
-            )
-
-            generated_text = response.text
-        except Exception as e:
-            logger.exception(f"Error during Gemini generation: {e}")
-            generated_text = "I apologize, but I encountered an error while generating a response."
-
-        logger.info(f"Generated output length: {len(generated_text)}.")
-        return {"response": generated_text,}
+   
