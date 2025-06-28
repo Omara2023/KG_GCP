05c0bdcca5808c53ff561a090758badbec8b5513
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-06-28 20:21:39 +0100

    Add vertex AI service.

---
+++ services/vertex_service.py
@@ -0,0 +1,13 @@
+import os
+from vertex_ai_caller import VertexAICaller
+
+class VertexService:
+    def __init__(self):
+        self.caller = VertexAICaller(
+            os.environ.get("GCP_PROJECT_ID"),
+            os.environ.get("GCP_VERTEX_LOCATION"),
+            os.environ.get("GCP_GEMINI_LOCATION") 
+        )
+
+    def search(self, query: str):
+        return self.caller.run_vertex_ai_search(query)
