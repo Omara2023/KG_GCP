2f28a64993ddc7cc09f6647aa512ca1685186c7d
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-07-22 16:29:00 +0100

    Re-implement context to LLM generation and output return to client-end.

--- routes/query.py
+++ routes/query.py
@@ -2,7 +2,7 @@ import logging
 from flask import Blueprint, request, jsonify
 from flask.views import MethodView
 from services.vertex_service import VertexRagService
-#from services.gemini_service import GeminiService
+from services.gemini_service import GeminiService
 
 logger = logging.getLogger(__name__)
 query_bp = Blueprint("query", __name__)
@@ -22,20 +22,15 @@ class QueryView(MethodView):
 
         logger.info(f"User query: '{user_query}'")
 
-        vertex_service = VertexRagService()
+        vertex_service = VertexRagService() 
         if (contexts := vertex_service.search(user_query)):
             logger.info("Successfully retrieved contexts from RAG engine.")
-            response_contexts = [ctx.to_dict() for ctx in contexts] 
-            return jsonify({"response": response_contexts}), 200
         else:
-            logger.info("Failed to retrieve contexts from RAG engine.")
-            return jsonify({"error": "Internal server error: failed to retrieve contexts"}), 500
-
+            logger.info("Failed to retrieve contexts from RAG engine.")     
         
-        
-        # gemini_service = GeminiService()
-        # response = gemini_service.respond(user_query, contexts)
+        gemini_service = GeminiService()
+        response = gemini_service.respond(user_query, contexts)
 
-        # return jsonify(response)
+        return jsonify({"response": response}), 200
     
 query_bp.add_url_rule('/query', view_func=QueryView.as_view("query_post"))
