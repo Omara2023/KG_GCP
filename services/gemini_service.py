69c59f9dfe2d2d04ec974deea0915dc7f7b14337
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-07-31 14:35:13 +0100

    Rewrite GeminiService to use new llm_behaviour wrappers.

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -3,19 +3,24 @@ from services.mixins import EnvMixin
 from clients.gemini_client import GeminiClient
 from models.context import RetrievedContext
 from models.llm_response import LLMResponse
+from llm_behaviours.grounded_generator import GroundedAnswerGenerator
+from llm_behaviours.step_back_rewriter import StepBackRewriter
 
 class GeminiService(EnvMixin):
     def __init__(self):
         self.project_id = self._get_env("GCP_PROJECT_ID")
         self.location = self._get_env("GCP_GEMINI_REGION")
         self.client = GeminiClient(project_id=self.project_id, location=self.location, model_name="gemini-1.5-flash")
+        self.grounded_generator =  GroundedAnswerGenerator(self.client)
+        self.step_back_rewriter = StepBackRewriter(self.client)
+        
         self.logger = logging.getLogger(__name__)
 
     def respond(self, query: str, contexts: list[RetrievedContext]) -> LLMResponse:
-        if not contexts:
-            self.logger.info("No relevant contexts found. Responding without grounding.")
-            return self.client.generate_response(query, [])
-        return self.client.generate_response(query, contexts)
+        return self.grounded_generator.generate(query, contexts)
+    
+    def rewrite_query(self, query: str) -> LLMResponse:
+        return self.step_back_rewriter.rewrite(query)
     
 def get_gemini_service() -> GeminiService:
     return GeminiService()
