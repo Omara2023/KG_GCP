605abc49c8eb35ed79290038e238b554809e9d34
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-08-05 14:58:10 +0100

    Refactor big query modules.

--- dependencies.py
+++ dependencies.py
@@ -8,6 +8,11 @@ from llm_behaviours.multi_query import MultiQueryExpander
 from llm_behaviours.grounded_generator import GroundedAnswerGenerator
 from services.gemini_service import GeminiService
 
+from clients.biq_query_client import BigQueryClient
+from services.big_query_service import BigQueryService
+
+#Gemini factories:
+
 def get_gemini_client() -> GeminiClient:
     project_id = os.getenv("GCP_PROJECT_ID")
     location = os.getenv("GCP_GEMINI_REGION")
@@ -31,3 +36,19 @@ def get_answer_generator(llm_client: GeminiClient = Depends(get_gemini_client)) 
 
 def get_gemini_service(rewriter: QueryRewriter = Depends(get_query_rewriter), generator: GroundedAnswerGenerator = Depends(get_answer_generator)) -> GeminiService:
     return GeminiService(rewriter, generator)
+
+#Vertex factories:
+
+#Big Query Factories:
+
+def get_big_query_client() -> BigQueryClient:
+    project_id=os.getenv("GCP_PROJECT_ID")
+    dataset_id=os.getenv("GCP_BQ_DATASET_ID")
+    table_id=os.getenv("GCP_BQ_TABLE_ID")
+    if project_id is None or dataset_id is None or table_id is None:
+        raise ValueError("Cannot instantiate BigQuery connection with missing env.")
+    
+    return BigQueryClient(project_id, dataset_id, table_id)
+
+def get_big_query_service(client: BigQueryClient = Depends(get_big_query_client)) -> BigQueryService:
+    return BigQueryService(client)
