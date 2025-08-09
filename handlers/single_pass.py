85e63e5a5ca50a1642f338224be1ec3c53b09a14
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-08-09 18:09:59 +0100

    Refactor single pass logic.

--- handlers/single_pass.py
+++ handlers/single_pass.py
@@ -1,18 +1,26 @@
 import asyncio
-from datetime import datetime
+import time
+from functools import wraps
 from typing import Callable
 from logging_modules.logging_config import setup_logging
 from services.vertex_service import VertexRagService
 from services.gemini_service import GeminiService
-from services.big_query_service import BigQueryService
-from models.log_entry import LogEntry
-from models.context import RetrievedContext
 from models.llm_response import LLMResponse
 
 
 logger = setup_logging()
 
-async def single_pass(query: str, vertex_service_factory: Callable[[], VertexRagService], rewrite_strategy: str, gemini_service: GeminiService, big_query_service: BigQueryService) -> LLMResponse:
+def log_duration(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        start = time.perf_counter()
+        result = func(*args, **kwargs)
+        duration = time.perf_counter() - start
+        #log request in new bq table
+        return result
+    return wrapper
+
+async def single_pass(query: str, vertex_service_factory: Callable[[], VertexRagService], rewrite_strategy: str, gemini_service: GeminiService) -> LLMResponse:
     """Single-pass RAG implementation: query -> rewrite -> retrieve -> generate."""
     rewritten_queries = gemini_service.rewrite_query(query)
 
@@ -28,13 +36,13 @@ async def single_pass(query: str, vertex_service_factory: Callable[[], VertexRag
     return response    
 
 
-def _log_to_big_query(service: BigQueryService, query: str, contexts: list[RetrievedContext], response: LLMResponse, start_time: datetime, rewritten_query: str|None, rewrite_strategy: str) -> None:
-    elapsed = (datetime.now()  - start_time).total_seconds()
-    log_entry = LogEntry(user_query=query, 
-                         retrieved_contexts=contexts,
-                         llm_output=response.text, 
-                         timestamp=datetime.now().isoformat(), 
-                         latency=elapsed,
-                         rewritten_query=rewritten_query,
-                         rewrite_strategy=rewrite_strategy)
-    service.log_query(log_entry)
+# def _log_to_big_query(service: BigQueryService, query: str, contexts: list[RetrievedContext], response: LLMResponse, start_time: datetime, rewritten_query: str|None, rewrite_strategy: str) -> None:
+#     elapsed = (datetime.now()  - start_time).total_seconds()
+#     log_entry = LogEntry(user_query=query, 
+#                          retrieved_contexts=contexts,
+#                          llm_output=response.text, 
+#                          timestamp=datetime.now().isoformat(), 
+#                          latency=elapsed,
+#                          rewritten_query=rewritten_query,
+#                          rewrite_strategy=rewrite_strategy)
+#     service.log_query(log_entry)
