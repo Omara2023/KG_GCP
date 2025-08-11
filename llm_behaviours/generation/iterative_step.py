14c3f3665b5e5335f23e4a89147388ef51498d1d
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-08-11 12:47:50 +0100

    Add iterative step generator wrapper to LLM behaviours.

---
+++ llm_behaviours/generation/iterative_step.py
@@ -0,0 +1,52 @@
+import json
+from typing import NoReturn
+from clients.gemini_client import GeminiClient
+from models.context import RetrievedContext
+from models.intermediate_llm_response import IntermediateLLMResponse
+
+class IterativeGenerator:
+    """Class responsible for the main augmented generation step."""  
+    
+    def __init__(self, llm_client: GeminiClient):
+        self.llm = llm_client
+
+    def generate(self, query: str, contexts: list[RetrievedContext]) -> IntermediateLLMResponse:
+        """Answer query using contexts, generating an intermediate answer and a subsequent question."""
+        if not contexts: raise ValueError("No contexts given to grounded answer generator.")        
+
+        output = self._ask_with_context(query, contexts)
+        response = self._safe_parse_json(output)
+        return response
+        
+    def _ask_with_context(self, user_query: str, contexts: list[RetrievedContext]) -> str:
+        """Construct prompt and invoke LLM."""
+        prompt_parts = ["Using the following contexts answer the question.\n"]
+        prompt_parts.append("--- Retrieved Contexts ---")
+        for i, context in enumerate(contexts):
+            prompt_parts.append(f"\nContext {i+1}:\nSource:{context.source_file}\nText:{context.text}")
+        prompt_parts.append("\n-------------------------\n")
+        prompt_parts.append("Answer the following question as accurately as possible and formulate a sensible subsequent query to be used for another iteration of rag-retrieval.")
+        prompt_parts.append("Your response should be formatted as json, with the keys: \"next_query\" and \"intermediate_answer\" respectively.")
+        prompt_parts.append(f"\n\nQuestion: {user_query}")
+        prompt = "\n".join(prompt_parts)
+        
+        return self.llm.prompt(prompt)
+    
+    def _safe_parse_json(self, text: str) -> IntermediateLLMResponse | NoReturn:
+        """Parse out desired JSON object from LLM response."""
+        try:
+            data = json.loads(text)
+        except json.JSONDecodeError:
+            start = text.find('{')
+            end = text.find('}')
+            if start != -1 and end != -1 and start < end:
+                try:
+                    data = json.loads(text[start:end+1])
+                except json.JSONDecodeError:
+                    raise ValueError("Could not parse JSON from response.")
+            else:
+                raise ValueError("Could not parse JSON from response.")
+            
+        if isinstance(data, dict) and "intermediate_answer" in data and "next_query" in data:
+            return IntermediateLLMResponse(intermediate_answer=data["intermediate_answer"], next_query=data["next_query"])
+        raise ValueError("Could not parse JSON from response.")
