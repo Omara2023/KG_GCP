1519b9de0b8ed63111969c9ccc974c537e054321
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-08-12 17:00:58 +0100

    Re-enable logging.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, Depends
 from dependencies import get_orchestrator 
 from orchestrators.base import BaseRAGOrchestrator
+from logging_modules.logging_config import setup_logging 
 from models.user_query import UserQuery
 from models.terminal_llm_response import TerminalLLMResponse
 
 app = FastAPI()
+setup_logging()
 
 @app.post("/query", response_model=TerminalLLMResponse)
 async def service_query(query: UserQuery, orchestrator: BaseRAGOrchestrator = Depends(get_orchestrator)):
