1dadbd9f31817f747f6e685314abbda7d6f67a35
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-09-01 17:38:28 +0100

    Generalise gemini_llm_client system_instruction.

--- clients/gemini_client.py
+++ clients/gemini_client.py
@@ -5,10 +5,11 @@ from google.genai import types
 class GeminiClient:
     """Wrapper class to generate responses using Gemini, grounded by the retrieved contexts."""
 
-    def __init__(self, project_id, location, model_name="gemini-1.5-flash"):
+    def __init__(self, project_id, location, system_instruction = None, model_name="gemini-1.5-flash"):
         self.client = genai.Client(vertexai=True, project=project_id, location=location)
         self.model_name = model_name
         self.logger = logging.getLogger(__name__)
+        self.system_instruction = system_instruction
 
     def prompt(self, prompt: str) -> str:
         try:
@@ -31,11 +32,6 @@ class GeminiClient:
         return types.GenerateContentConfig(
             temperature=0.1,
             max_output_tokens=4000,
-            system_instruction=[
-                "You are an expert assistant. Use the retrieved contexts provided to answer the user's question as faithfully as possible. "
-                "Do not invent facts or speculate. If the contexts contain relevant information, use them. "
-                "If the contexts do not include enough information to answer the question fully, you may rely on your own general knowledge to fill in the gaps, "
-                "but clearly indicate when this is the case. If the question cannot be answered with the context or your own knowledge, say so."
-            ]
+            system_instruction=self.system_instruction
         )
    
--- services/retrieval_service.py
+++ services/retrieval_service.py
@@ -2,6 +2,12 @@ from clients.vertex_retrieval_client import VertexRetrievalClient
 from clients.gemini_client import GeminiClient
 
 async def single_pass(query: str, retriever: VertexRetrievalClient, llm_client: GeminiClient) -> str:
+    llm_client.system_instruction = [
+        "You are an expert assistant. Use the retrieved contexts provided to answer the user's question as faithfully as possible. "
+        "Do not invent facts or speculate. If the contexts contain relevant information, use them. "
+        "If the contexts do not include enough information to answer the question fully, you may rely on your own general knowledge to fill in the gaps, "
+        "but clearly indicate when this is the case. If the question cannot be answered with the context or your own knowledge, say so."
+    ]
     contexts = [c.text for c in retriever.run_context_retrieval(query)]
     prompt = _format_prompt(query, contexts)
     return llm_client.prompt(prompt)
