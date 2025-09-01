720852607da4ccbde48f484250fd3416e50c0436
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-09-01 17:00:19 +0100

    Remove terminalLLMResponse model.

--- models/terminal_llm_response.py
+++
@@ -1,8 +0,0 @@
-from models.llm_response import LLMResponse
-
-class TerminalLLMResponse(LLMResponse):
-    """Final responses from LLM to be returned to user."""
-    text: str
-
-    def to_user_friendly(self) -> str:
-        return self.text
