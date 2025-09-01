5873edf9722626aedfbce8fd4a51fb5b2f18c8fa
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-09-01 17:00:47 +0100

    Use simplified handler in monoroute.

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from fastapi import FastAPI, Depends
-from dependencies import get_orchestrator 
-from orchestrators.base import BaseRAGOrchestrator
 from logging_modules.logging_config import setup_logging 
 from models.user_query import UserQuery
-from models.terminal_llm_response import TerminalLLMResponse
+from services.retrieval_service import single_pass
+from dependencies import get_gemini_client, get_vertex_retrieval_client
+from clients.vertex_retrieval_client import VertexRetrievalClient
+from clients.gemini_client import GeminiClient
 
 app = FastAPI()
 setup_logging()
 
-@app.post("/query", response_model=TerminalLLMResponse)
-async def service_query(query: UserQuery, orchestrator: BaseRAGOrchestrator = Depends(get_orchestrator)):
-    return await orchestrator.generate(query.text)
-    
-         
-
+@app.post("/query")
+async def service_query(
+    query: UserQuery,
+    retriever: VertexRetrievalClient = Depends(get_vertex_retrieval_client),
+    llm_client: GeminiClient = Depends(get_gemini_client)
+):
+    text = await single_pass(query.text, retriever, llm_client)
+    return {"text": text}
