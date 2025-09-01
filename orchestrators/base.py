64947c21126c372ec2eb0625232252dca7543c2d
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-09-01 16:59:42 +0100

    Remove orchestrator pattern.

--- orchestrators/base.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from models.llm_response import LLMResponse
-
-class BaseRAGOrchestrator(ABC):
-    """Interface for a complete RAG-pipeline."""
-
-    @abstractmethod
-    async def generate(self, query: str) -> LLMResponse: #consider changing return type to terminalllmresponse
-        pass
