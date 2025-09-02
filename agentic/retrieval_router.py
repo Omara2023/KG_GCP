80a8ae992f4a0b3df02309becab418364269b1f7
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-09-02 15:43:29 +0100

    Implement basic functionality of retrieval_router.

--- agentic/retrieval_router.py
+++ agentic/retrieval_router.py
@@ -1,31 +1,29 @@
 from json import loads
-from enums.retriever_types import RetrieverTypes
+from typing import Any
 from clients.gemini_client import GeminiClient
+from clients.vertex_retrieval_client import VertexRetrievalClient
+#future types: simple, hydrid, keyword???, answer_in_question...
 
 class RetrievalRouter:
     """Decide which retrieval strategy should be used for a query."""
-    def __init__(self, llm_client: GeminiClient):
+    def __init__(self, retrievers: dict[str, Any], llm_client: GeminiClient):
+        self.retrievers = retrievers
         self.llm = llm_client
         self.llm.system_instruction = "You are an expert decision maker, deciding what type of RAG lookup strategy to use in order to answer user queries factually and usefully."
-
-    def get_retriever(self, query: str) ->  RetrieverTypes:
+        
+    def get_retriever(self, query: str) -> VertexRetrievalClient: #refactor to have a family of retriever classes implementing a retiver interface
         prompt = self._contruct_prompt(query)
         output = self.llm.prompt(prompt)
         option = loads(output)["choice"]
-        return self._string_to_retriever_type(option)
+        if option not in self.retrievers:
+            raise ValueError("Incorrect retriever type returned by LLM.")
+        return self.retrievers[option]
     
     def _contruct_prompt(self, query: str) -> str:
         return (
             "Decide which retrieval strategy is best suited for a RAG based system to answer the user query.\n"
             "Types:\n"
-            f"{', '.join(f'RetrieverTypes.{i.value}' for i in RetrieverTypes)}\n"
+            f"{', '.join(f'{i}' for i in self.retrievers.keys())}\n"
             f"User query: {query}\n"
-            "Return your answer as JSON with key 'choice' and value RetrieverTypes.<TYPE>"
+            "Return your answer as JSON with key 'choice' and value <TYPE>"
         )
-
-    def _string_to_retriever_type(self, input: str) -> RetrieverTypes:
-        match input:
-            case RetrieverTypes.SIMPLE.value:
-                return RetrieverTypes.SIMPLE
-            case _:
-                raise ValueError("Invalid retriever type returned from LLM.")
