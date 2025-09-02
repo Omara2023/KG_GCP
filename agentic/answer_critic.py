21d0998921e8f45f1f786f732ab7f1a8d16b1344
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-09-02 12:32:59 +0100

    Implement answer_critic class.

---
+++ agentic/answer_critic.py
@@ -0,0 +1,30 @@
+from json import loads, JSONDecodeError
+from clients.gemini_client import GeminiClient
+
+class AnswerCritic:
+    """Evaluates proposed answer to query and coordinates next action."""
+
+    def __init__(self, llm_client: GeminiClient):
+        self.llm_client = llm_client
+        self.llm_client.system_instruction = "You are an expert in world knowledge. You will be assessing whether a response answers the given question in a satisfactory manner."
+
+    def judge(self, query: str, answer: str) -> dict:
+        """LLM decides whether or not the query has been sufficiently answered."""
+        prompt = self._construct_prompt(query, answer)
+        output = self.llm_client.prompt(prompt)
+        try:
+            return loads(output)
+        except JSONDecodeError as e:
+            print(e, flush=True)
+            return {}
+
+    def _construct_prompt(self, query: str, answer: str) -> str:
+        return (
+            "Decide whether this answer properly answers this query or not.\n"
+            f"Query: {query}\n"
+            f"Proposed Answer: {answer}\n "
+            "If the answer is satisfactory you will reply back with JSON 'verdict': 'satisfactory'."
+            "Should the answer not be satisfactory you will reply with JSON object 'verdict: 'unsatisfactory' and 'follow_up': '<QUERY>',"
+            "where <QUERY> is a follow up question of your choosing that is best suited for another round of retrieval in an attempt to answer the original query."
+            "To reiterate, your answer should be a formatted JSON object that py's json.loads() can process directly."
+        )
