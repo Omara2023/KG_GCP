e9c0b2a469712baa02d0b16dbe909348dd77ac3d
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-09-08 12:28:15 +0100

    Refactor multi and subquery to leverage new interface for json_list parsing.

--- llm_behaviours/pre_retrieval/multi_query.py
+++ llm_behaviours/pre_retrieval/multi_query.py
@@ -1,9 +1,8 @@
-import json
 import logging
-from llm_behaviours.pre_retrieval.base import QueryRewriter
+from llm_behaviours.pre_retrieval.multiple import MultipleOutput
 from clients.gemini_client import GeminiClient
 
-class MultiQueryExpander(QueryRewriter):
+class MultiQueryExpander(MultipleOutput):
     """Class responsible for generate variations of the original query."""  
 
     DESCRIPTION = "Generate n variations of the query."
@@ -22,22 +21,3 @@ class MultiQueryExpander(QueryRewriter):
             f"Generate {n} variations of the following query:{query}:\n"
             f"Respond only with a valid JSON array of objects with a single query key. No explanations."
         )
-
-    def _safe_parse_json_list(self, text: str) -> list[str]:  #TODO refactor subquery and multi-query to either use inheritance or mixin for json loading list method
-        try:
-            data = json.loads(text)
-        except json.JSONDecodeError:
-            start = text.find('[')
-            end = text.find(']')
-            if start != -1 and end != -1 and start < end:
-                try:
-                    data = json.loads(text[start:end+1])
-                except json.JSONDecodeError:
-                    raise ValueError("Could not parse JSON from response.")
-            else:
-                raise ValueError("Could not parse JSON from response.")
-
-        if isinstance(data, list) and all(isinstance(item, dict) and 'query' in item for item in data):
-            return [item["query"] for item in data]
-        else:
-            raise ValueError("Parsed JSON structure unexpected.")
--- llm_behaviours/pre_retrieval/sub_query.py
+++ llm_behaviours/pre_retrieval/sub_query.py
@@ -1,9 +1,8 @@
-import json
 import logging
-from llm_behaviours.pre_retrieval.base import QueryRewriter
+from llm_behaviours.pre_retrieval.multiple import MultipleOutput
 from clients.gemini_client import GeminiClient
 
-class SubQueryExpander(QueryRewriter):
+class SubQueryExpander(MultipleOutput):
     """Class responsible for generating decomposed parts of the original query."""  
     
     DESCRIPTION = "Break down the query into n sub-queries targeting different aspects of the query."
@@ -15,7 +14,7 @@ class SubQueryExpander(QueryRewriter):
     def rewrite(self, query: str, n = 5) -> list[str]:
         prompt = self._prompt(query, n)
         output = self.llm.prompt(prompt)
-        return self._safe_parse_json_list(output) #TODO refactor subquery and multi-query to either use inheritance or mixin
+        return self._safe_parse_json_list(output) 
     
     def _prompt(self, query: str, n: int) -> str:
         return (
@@ -23,21 +22,4 @@ class SubQueryExpander(QueryRewriter):
             f"Respond only with a valid JSON array of objects with a single query key. No explanations."
         )
 
-    def _safe_parse_json_list(self, text: str) -> list[str]:
-        try:
-            data = json.loads(text)
-        except json.JSONDecodeError:
-            start = text.find('[')
-            end = text.find(']')
-            if start != -1 and end != -1 and start < end:
-                try:
-                    data = json.loads(text[start:end+1])
-                except json.JSONDecodeError:
-                    raise ValueError("Could not parse JSON from response.")
-            else:
-                raise ValueError("Could not parse JSON from response.")
-
-        if isinstance(data, list) and all(isinstance(item, dict) and 'query' in item for item in data):
-            return [item["query"] for item in data]
-        else:
-            raise ValueError("Parsed JSON structure unexpected.")
+    
