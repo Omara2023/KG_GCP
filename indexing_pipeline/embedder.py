1cb7441776b993c1d04fd32466dcdab93a6c42df
Omar Al-Mizan <144819892+Omara2023@users.noreply.github.com>
2025-09-22 17:55:10 +0100

    Simplify embedder by removing FAISS.

--- indexing_pipeline/embedder.py
+++ indexing_pipeline/embedder.py
@@ -1,20 +1,13 @@
-import numpy as np
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_community.vectorstores import FAISS
-from langchain.schema import Document
 
 class Embedder:
     """Class to turn chunks into dense vectors via embedding model."""
 
-    def __init__(self, model_name="all-mpnet-base-v2"):
-        self.embedding_model = HuggingFaceEmbeddings(model_name=model_name)
+    def __init__(self, model_name="all-mpnet-base-v2"): #TODO - change this to use dependency injections, preparing a set embedder to pass in and env based model name.
+        self.embedder = HuggingFaceEmbeddings(model_name=model_name)
 
-    def embed_chunks(self, chunks: list[Document]) -> np.ndarray:
-        faiss = FAISS.from_documents(chunks, self.embedding_model)
-        return self._faiss_to_array(faiss)
+    def embed_chunks(self, chunks: list[str]) -> list[list[float]]:
+        return self.embedder.embed_documents(chunks)
     
-    def _faiss_to_array(self, faiss: FAISS) -> np.ndarray:
-        index = faiss.index
-        ntotal = index.ntotal
-        vectors = [index.reconstruct(i) for i in range(ntotal)]
-        return np.array(vectors)
+    def embed_query(self, query: str) -> list[float]:
+        return self.embed_query(query)
